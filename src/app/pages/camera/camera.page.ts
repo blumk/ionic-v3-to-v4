133e3358682ad58cf1e26a4ce5c6892b2f7b307f
daviddalbusco <david.dalbusco@outlook.com>
2018-07-01 16:29:18 +0200

    Call gallery

--- src/app/pages/camera/camera.page.ts
+++ src/app/pages/camera/camera.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Camera, CameraOptions, CameraPopoverOptions} from '@ionic-native/camera';
 
 @Component({
   selector: 'camera',
@@ -7,12 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CameraPage implements OnInit {
 
-  
+  image: string;
 
-  constructor() { }
+  constructor(private camera: Camera) {
+  }
 
   ngOnInit() {
-    
+
+  }
+
+  pickPhoto() {
+    const popoverOptions: CameraPopoverOptions = {
+      x: 0,
+      y: 0,
+      width: 1080,
+      height: 1080,
+      arrowDir: this.camera.PopoverArrowDirection.ARROW_ANY
+    };
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      allowEdit: false,
+      encodingType: this.camera.EncodingType.JPEG,
+      targetWidth: 1080,
+      targetHeight: 1080,
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true,
+      saveToPhotoAlbum: false,
+      cameraDirection: this.camera.Direction.BACK,
+      popoverOptions: popoverOptions
+    };
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      this.image = null;
+    });
   }
 
 }
