5f0054227c3246299fd02884df147c8826eb1280
daviddalbusco <david.dalbusco@outlook.com>
2018-07-01 14:28:03 +0200

    Navigate to camera page with an action sheet

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {ActionSheetController, NavController} from 'ionic-angular';
+import {CameraPage} from '../camera/camera';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private navCtrl: NavController,
+              private actionSheetCtrl: ActionSheetController) {
 
   }
 
+  openActionSheet() {
+    const actionSheet = this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Navigate to camera page',
+          role: 'destructive',
+          handler: () => {
+            this.navCtrl.push(CameraPage);
+          }
+        }, {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            // Do nothing
+          }
+        }
+      ]
+    });
+    actionSheet.present().then(() => {
+      // Do nothing
+    });
+  }
 }
