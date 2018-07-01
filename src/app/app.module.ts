434b39d3c4fa6eb9795d63b2910262423a20947c
daviddalbusco <david.dalbusco@outlook.com>
2018-07-01 16:44:18 +0200

    Fix module import camera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,11 @@ import {RouteReuseStrategy} from '@angular/router';
 import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
+import {Camera} from '@ionic-native/camera/ngx';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {ComponentsModule} from './components/components.module';
-import {Camera} from '@ionic-native/camera';
 
 @NgModule({
   declarations: [AppComponent],
